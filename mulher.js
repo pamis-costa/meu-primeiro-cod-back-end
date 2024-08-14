f5957b500cd02f6cede265b05cf1e10ad0750b60
pamis-costa <pamelapereira321@gmail.com>
2024-08-14 12:13:18 -0300

    insira a nova versao do projeto integrado ao mongoDB

---
+++ mulher.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const router = express.Router()
+
+const app = express()
+const porta = 3333
+
+function mostraMulher(request, response){
+    response.json({
+        nome: "Pâmela Costa",
+        imagem: "https://avatars.githubusercontent.com/u/124310748?v=4",
+        minibio: "Desenvolvedora e Analista"
+
+    })
+}
+
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta", porta)
+}
+
+app.use(router.get("/mulher", mostraMulher))
+app.listen(porta, mostraPorta)
